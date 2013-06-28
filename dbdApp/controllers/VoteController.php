61ca19800d8c02bc47a8a1abb863f2efc80e8ebb
dubhunter <get@willmason.me>
2013-06-28 14:30:56 -0700

    make sure we have a phone number

--- dbdApp/controllers/VoteController.php
+++ dbdApp/controllers/VoteController.php
@@ -3,39 +3,47 @@ class VoteController extends SVController {
 
 	public function doPost() {
 		dbdLog($this->getParams());
-		$to = $this->getParam('To');
-		$from = $this->getParam('From');
-		$vote = $this->getParam('Body');
+		try {
+			$to = $this->getParam('To');
+			$from = $this->getParam('From');
+			$vote = $this->getParam('Body');
 
-		$phoneNumber = PhoneNumber::getByDid($to);
+			$phoneNumber = PhoneNumber::getByDid($to);
 
-		$voteMax = $phoneNumber->getVoteMax();
-		$validMin = $phoneNumber->getValidMin();
-		$validMax = $phoneNumber->getValidMax();
-
-		$count = Vote::getCount(array(
-			'to' => $to,
-			'from' => $from,
-		));
-
-		$this->view->assign('voter', $from);
-
-		if (is_numeric($vote) && $vote >= $validMin && $vote <= $validMax) {
-			if ($count < $voteMax) {
-				$V = new Vote();
-				$V->setTo($to);
-				$V->setFrom($from);
-				$V->setVote($vote);
-				$V->save();
-				$this->setTemplate('twiml-thanks.tpl');
-			} else {
-				$this->view->assign('voteMax', $voteMax);
-				$this->setTemplate('twiml-max-votes.tpl');
+			if (!$phoneNumber) {
+				throw new SVException(SVException::BAD_REQUEST);
 			}
-		} else {
-			$this->view->assign('validMin', $validMin);
-			$this->view->assign('validMax', $validMax);
-			$this->setTemplate('twiml-invalid.tpl');
+
+			$voteMax = $phoneNumber->getVoteMax();
+			$validMin = $phoneNumber->getValidMin();
+			$validMax = $phoneNumber->getValidMax();
+
+			$count = Vote::getCount(array(
+				'to' => $to,
+				'from' => $from,
+			));
+
+			$this->view->assign('voter', $from);
+
+			if (is_numeric($vote) && $vote >= $validMin && $vote <= $validMax) {
+				if ($count < $voteMax) {
+					$V = new Vote();
+					$V->setTo($to);
+					$V->setFrom($from);
+					$V->setVote($vote);
+					$V->save();
+					$this->setTemplate('twiml-thanks.tpl');
+				} else {
+					$this->view->assign('voteMax', $voteMax);
+					$this->setTemplate('twiml-max-votes.tpl');
+				}
+			} else {
+				$this->view->assign('validMin', $validMin);
+				$this->view->assign('validMax', $validMax);
+				$this->setTemplate('twiml-invalid.tpl');
+			}
+		} catch (SVException $e) {
+			$this->e($e);
 		}
 	}
 }
