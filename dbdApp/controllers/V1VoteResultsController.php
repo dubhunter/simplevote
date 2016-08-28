ba705bd9c0604f4660126dd3c515d830cc5a3cac
dubhunter <get@willmason.me>
2016-08-28 01:24:10 -0700

    add support for multiple numbers

--- dbdApp/controllers/V1VoteResultsController.php
+++ dbdApp/controllers/V1VoteResultsController.php
@@ -4,22 +4,26 @@ class V1VoteResultsController extends V1ApiController {
 
 	public function doGet() {
 		try {
-			$did = '+' . $this->getParam('to');
+			foreach (explode(',', $this->getParam('to')) as $to) {
+				$did = '+' . $to;
 
-			$phoneNumber = PhoneNumber::getByDid($did);
+				$phoneNumber = PhoneNumber::getByDid($did);
 
-			for ($i = $phoneNumber->getValidMin(); $i <= $phoneNumber->getValidMax(); $i++) {
-				$this->data[$i] = 0;
-			}
+				for ($i = $phoneNumber->getValidMin(); $i <= $phoneNumber->getValidMax(); $i++) {
+					if (!isset($this->data[$i])) {
+						$this->data[$i] = 0;
+					}
+				}
 
-			/** @var Vote[] $votes */
-			$votes = Vote::getAll(array(
-				'to' => $did,
-			));
+				/** @var Vote[] $votes */
+				$votes = Vote::getAll(array(
+					'to' => $did,
+				));
 
-			foreach ($votes as $vote) {
-				$key = $vote->getVote();
-				$this->data[$key]++;
+				foreach ($votes as $vote) {
+					$key = $vote->getVote();
+					$this->data[$key]++;
+				}
 			}
 		} catch (SVException $e) {
 			$this->e($e);
